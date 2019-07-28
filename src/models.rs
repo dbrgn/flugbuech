4843b2d668ae1671f33954bce2d4ffffe97be564
Danilo Bargen <mail@dbrgn.ch>
2019-07-28 16:38:06 +0200

    WIP

--- src/models.rs
+++ src/models.rs
@@ -1,7 +1,8 @@
+use chrono::{DateTime, offset::Utc};
 use diesel::{Associations, Identifiable, Queryable};
 use serde::Serialize;
 
-use crate::schema::{users, aircraft};
+use crate::schema::{users, aircraft, locations, flights};
 
 #[derive(Identifiable, Queryable, Serialize, PartialEq, Debug, Clone)]
 #[table_name="users"]
@@ -21,4 +22,50 @@ pub struct Aircraft {
     pub user_id: i32,
     pub model: String,
     pub manufacturer: String,
+}
+
+#[derive(Identifiable, Queryable, Serialize, PartialEq, Debug, Clone)]
+#[table_name="locations"]
+pub struct Location {
+    pub id: i32,
+    pub name: String,
+    pub country: String,
+    pub elevation: i32,
+}
+
+#[derive(Identifiable, Queryable, Associations, Serialize, PartialEq, Debug, Clone)]
+#[belongs_to(User, foreign_key="user_id")]
+#[belongs_to(Aircraft, foreign_key="aircraft_id")]
+#[belongs_to(Location, foreign_key="launch_at")]
+#[belongs_to(Location, foreign_key="landing_at")]
+#[table_name="flights"]
+pub struct Flight {
+    /// Primary key
+    pub id: i32,
+    /// The user-defined flight number
+    pub number: i32,
+    /// The pilot
+    pub user_id: i32,
+    /// The aircraft
+    pub aircraft_id: Option<i32>,
+    /// Launch location
+    pub launch_at: Option<i32>,
+    /// Landing location
+    pub landing_at: Option<i32>,
+    /// Time of launch
+    pub launch_time: Option<DateTime<Utc>>,
+    /// Time of landing
+    pub landing_time: Option<DateTime<Utc>>,
+    /// GPS track length
+    pub track_distance: Option<f32>,
+    /// XContest tracktype (free_flight, flat_triangle or fai_triangle)
+    pub xcontest_tracktype: Option<String>,
+    /// XContest distance
+    pub xcontest_distance: Option<f32>,
+    /// XContest URL
+    pub xcontest_url: Option<String>,
+    /// Comment your flight
+    pub comment: Option<String>,
+    /// Link to a video of your flight
+    pub video_url: Option<String>,
 }
--- src/schema.rs
+++ src/schema.rs
@@ -8,6 +8,34 @@ table! {
 }
 
 table! {
+    flights (id) {
+        id -> Int4,
+        number -> Nullable<Int4>,
+        user_id -> Int4,
+        aircraft_id -> Nullable<Int4>,
+        launch_at -> Nullable<Int4>,
+        landing_at -> Nullable<Int4>,
+        launch_time -> Nullable<Timestamptz>,
+        landing_time -> Nullable<Timestamptz>,
+        track_distance -> Nullable<Float4>,
+        xcontest_tracktype -> Nullable<Text>,
+        xcontest_distance -> Nullable<Float4>,
+        xcontest_url -> Nullable<Text>,
+        comment -> Nullable<Text>,
+        video_url -> Nullable<Text>,
+    }
+}
+
+table! {
+    locations (id) {
+        id -> Int4,
+        name -> Text,
+        country -> Bpchar,
+        elevation -> Int4,
+    }
+}
+
+table! {
     users (id) {
         id -> Int4,
         username -> Varchar,
@@ -16,8 +44,12 @@ table! {
 }
 
 joinable!(aircraft -> users (user_id));
+joinable!(flights -> aircraft (aircraft_id));
+joinable!(flights -> users (user_id));
 
 allow_tables_to_appear_in_same_query!(
     aircraft,
+    flights,
+    locations,
     users,
 );
