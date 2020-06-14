bff676a5bd81454bcd3d81b0dd84fec4c7fdfed2
Danilo Bargen <mail@dbrgn.ch>
2020-06-14 23:24:37 +0200

    When location coordinates change, update map

--- static/svelte/Map.svelte
+++ static/svelte/Map.svelte
@@ -24,6 +24,23 @@
         map.setStyle(`mapbox://styles/mapbox/${style}`);
     }
 
+    // Helper function to validate a coordinate pair
+    function validLngLat(lng: number | null, lat: number | null): boolean {
+        if (lng === null || lat === null) {
+            return false;
+        }
+        if (isNaN(lng) || isNaN(lat)) {
+            return false;
+        }
+        if (lng < -180 || lng > 180) {
+            return false;
+        }
+        if (lat < -90 || lat > 90) {
+            return false;
+        }
+        return true;
+    }
+
     onMount(() => {
         // Create map
         map = new Map({
@@ -58,6 +75,19 @@
             marker.setLngLat(e.lngLat);
             updateCoordinatesFromMarker();
         });
+
+        // When the input value changes, update the marker
+        const updateMarkerFromCoordinates = () => {
+            const lng = parseFloat(lngInput.value);
+            const lat = parseFloat(latInput.value);
+            if (validLngLat(lng, lat) === true) {
+                const pos = {lng: lng, lat: lat};
+                marker.setLngLat(pos);
+                map.flyTo({center: pos});
+            }
+        };
+        lngInput.addEventListener('change', updateMarkerFromCoordinates);
+        latInput.addEventListener('change', updateMarkerFromCoordinates);
     });
 </script>
 
