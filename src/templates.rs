3081963640bac6a47f043d82f65c0d33d8e0a12d
Danilo Bargen <mail@dbrgn.ch>
2021-04-18 22:39:43 +0200

    Enable tera autoescaping for .tera files

--- src/templates.rs
+++ src/templates.rs
@@ -5,6 +5,10 @@ use crate::filters;
 
 pub fn fairing() -> impl Fairing {
     Template::custom(|engines| {
+        // Autoescape HTML and Tera files
+        engines.tera.autoescape_on(vec![".html", ".tera"]);
+
+        // Register custom filters
         engines.tera.register_filter("duration", filters::duration);
         engines
             .tera
