7ca747403e3d11a490b86e53f738c2a3a4e01a65
Danilo Bargen <mail@dbrgn.ch>
2023-12-30 22:51:28 +0100

    Add CountryFlag component

---
+++ frontend/src/lib/components/CountryFlag.svelte
@@ -0,0 +1,23 @@
+<!-- @component
+Render a country flag using the FlagMeister SVG library.
+-->
+<script lang="ts">
+  /**
+   * The two-letter country code.
+   */
+  export let countryCode: string;
+</script>
+
+<!-- Custom elements from FlagMeister: E.g. <flag-ch></flag-ch> -->
+<svelte:element this={`flag-${countryCode.toLowerCase()}`} class="flag"></svelte:element>
+
+<style>
+  .flag {
+    display: inline-block;
+  }
+
+  .flag :global(img) {
+    width: 1em;
+    height: 0.8em;
+  }
+</style>
