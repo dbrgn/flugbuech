592af603f6d9a1c36180b93053444c5da5b721b7
Danilo Bargen <mail@dbrgn.ch>
2025-09-06 00:38:44 +0200

    Re-add plausible integration

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,5 +7,23 @@ cp -Rv /flugbuech/static/* /static/
 find /static -type f -exec chmod a+r {} \;
 find /static -type d -exec chmod a+rx {} \;
 
+# Enable statistics if enabled through env var
+insert_snippet() {
+  local file="$1"
+  local domain="$2"
+  local url="$3"
+
+  local marker="<!--end-of-head-->"
+  local line1="<!-- self-hosted, GDPR compliant, non-privacy-invading visitor stats -->"
+  local line2="<script defer data-domain=\"${domain}\" src=\"${url}\"></script>"
+
+  sed -i "/${marker}/i $line1" "$file"
+  sed -i "s|${marker}|$line2|" "$file"
+}
+if [[ -n "${PLAUSIBLE_DOMAIN:-}" && -n "${PLAUSIBLE_URL:-}" ]]; then
+  insert_snippet "/static/index.html" "$PLAUSIBLE_DOMAIN" "$PLAUSIBLE_URL"
+  insert_snippet "/static/fallback.html" "$PLAUSIBLE_DOMAIN" "$PLAUSIBLE_URL"
+fi
+
 # Run API
 exec ./flugbuech-api --migrate
