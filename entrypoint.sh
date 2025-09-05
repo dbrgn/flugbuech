bbb3db30dbde4c57e692400c1f186f9546f60a55
Danilo Bargen <mail@dbrgn.ch>
2025-09-05 19:14:20 +0200

    Add nginx container to docker-compose.yml

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,8 @@ set -euo pipefail
 # Copy static files to volume
 rm -rf /static/*
 cp -Rv /flugbuech/static/* /static/
+find /static -type f -exec chmod a+r {} \;
+find /static -type d -exec chmod a+rx {} \;
 
 # Run API
 exec ./flugbuech-api --migrate
